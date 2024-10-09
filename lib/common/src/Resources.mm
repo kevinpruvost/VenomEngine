a3694fb9261fea116a0a489891ea909ba9298fd7
Kevin Pruvost <kevin.pruvost@epitech.eu>
2024-10-09 19:37:01 +0800

    removing glm to switch to alusch fork as he has neon-compile-error branch

---
+++ lib/common/src/Resources.mm
@@ -0,0 +1,17 @@
+///
+/// Project: VenomEngine
+/// @file Resources.cc
+/// @date Aug, 22 2024
+/// @brief
+/// @author Pruvost Kevin | pruvostkevin (pruvostkevin0@gmail.com)
+///
+#include <venom/common/Resources.h>
+
+std::string getResourcePath() {
+    // Get the path to the resource directory in the application bundle
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *resourcePath = [bundle resourcePath];
+
+    // Convert NSString to std::string
+    return std::string([resourcePath UTF8String]);
+}
